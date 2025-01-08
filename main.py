55432263d5d0c7c4f69552830853751e60540414
Zuzanna Zimmer <114940033+ZuzannaZimmer@users.noreply.github.com>
2025-01-08 16:13:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+
+if __name__ == "__main__":
+    try:
+        print(f"Host: {os.environ['DB__HOST']!r}")
+        print(f"Name: {os.environ['DB__DBNAME']!r}")
+        print(f"Username: {os.environ['DB__USER']!r}")
+        print(f"Password: {os.environ['DB__PASSWORD']!r}")
+        print(f"Engine: {os.environ['DB__ENGINE']!r}")
+
+    except KeyError:
+        print("Environment variables not set!")
